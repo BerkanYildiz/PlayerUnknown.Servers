3aab8f5922f95797825d3e6d7a57b6e83ebcc2bb
Berkan YILDIZ <BerkanYildiz@users.noreply.github.com>
2018-02-17 02:28:50 +0100

    some translations

---
+++ PlayerUnknown/Logic/Components/Inventories/InventoryObject.cs
@@ -0,0 +1,13 @@
+﻿namespace PlayerUnknown.Logic.Components.Inventories
+{
+    public class InventoryObject
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InventoryObject"/> class.
+        /// </summary>
+        public InventoryObject()
+        {
+            // InventoryObject.
+        }
+    }
+}
---
+++ PlayerUnknown/Logic/Components/Inventories/Item.cs
@@ -0,0 +1,29 @@
+﻿namespace PlayerUnknown.Logic.Components.Inventories
+{
+    public sealed class Item : InventoryObject
+    {
+        public string ItemDescId;
+        public string PartDescId;
+        public string Name;
+        public string Desc;
+        public string PresetId;
+        public string Quality;
+
+        public int Count;
+        public int BuyPrice;
+        public int SellPrice;
+        public int PriceInCents;
+        public int WeeklyPurchaseLimit;
+
+        public bool InEquip;
+        public bool Doubling;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Item"/> class.
+        /// </summary>
+        public Item()
+        {
+            // Item.
+        }
+    }
+}
